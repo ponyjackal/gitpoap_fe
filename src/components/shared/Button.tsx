d18c7b3dc30b9ecd88bdf06b7354bcfeb7ea9445
ponyjackal <pony1125@outlook.com>
2022-02-02 19:30:56 -0500

    Create basic new button with mantine

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,38 +1,38 @@
-import React from "react";
-import styled from "styled-components";
-import MUButton from "@mui/material/Button";
-import { rem } from "polished";
-import { PrimaryBlue, PrimaryBlueHover } from "../../colors";
+import React from 'react';
+import styled from 'styled-components';
+import { Button as MButton } from '@mantine/core';
+import { rem } from 'polished';
+import { PrimaryBlue, TextAccent } from '../../colors';
 
 type Props = {
   children: React.ReactNode;
   className?: string;
   onClick?: React.MouseEventHandler;
   disabled?: boolean;
-  endIcon?: React.ReactNode;
+  leftIcon?: React.ComponentProps<typeof MButton>['leftIcon'];
 };
 
-const StyledButton = styled(MUButton)`
+const StyledButton = styled(MButton)`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   background-color: ${PrimaryBlue};
-  font-family: "Inter", sans-serif;
-  font-weight: 500;
+  font-family: 'PT Mono', monospace;
+  font-weight: 700;
   color: white;
-  font-size: ${rem(18)};
-  line-height: ${rem(22)};
-  width: ${rem(140)};
-  height: ${rem(50)};
+  text-transform: uppercase;
+  font-size: ${rem(12)};
+  line-height: ${rem(18)};
+  letter-spacing: ${rem(2)};
+  transition: 200ms background ease;
   border-radius: ${rem(6)};
-  padding: ${rem(12)} ${rem(16)};
-  text-transform: none;
-  box-shadow: none;
+  padding: ${rem(8)} ${rem(14)};
+  height: auto;
 
   &:hover {
     box-shadow: none;
-    background-color: ${PrimaryBlueHover};
+    background-color: ${TextAccent};
   }
 
   &:active {
@@ -40,20 +40,15 @@ const StyledButton = styled(MUButton)`
   }
 `;
 
-export const Button = ({
-  children,
-  className,
-  endIcon,
-  onClick,
-  disabled,
-}: Props) => {
+export const Button = ({ children, className, onClick, disabled, leftIcon }: Props) => {
   return (
     <StyledButton
+      compact
       className={className}
+      size="md"
       disabled={disabled}
-      endIcon={endIcon}
       onClick={onClick}
-      variant="contained"
+      leftIcon={leftIcon}
     >
       {children}
     </StyledButton>
