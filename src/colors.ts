177cccd904b12f07f123ba4f812c07a5a3875441
ponyjackal <pony1125@outlook.com>
2022-02-04 17:22:11 -0500

    add new colors

--- src/colors.ts
+++ src/colors.ts
@@ -24,6 +24,8 @@ export const Gray5 = '#E8EDFA';
 export const Gray6 = '#E3EBF6';
 
 /* -------- DeepWork Colors ------------*/
+/* Proposition to use real color names, and use those when setting a theme */
+export const DarkGray = '#393A51';
 export const MidnightBlue = '#10111E';
 
 export const BackgroundPanel = '#1E1F2E';
@@ -31,3 +33,8 @@ export const BackgroundPanel = '#1E1F2E';
 export const TextLight = '#E2E2EE';
 export const TextAccent = PrimaryBlue;
 export const TextGray = '#838495';
+export const TextDarkGray = DarkGray;
+
+export const ExtraHover = '#5596F6';
+export const ExtraActive = '#1761CF';
+export const ExtraPressed = '#1761CF';
