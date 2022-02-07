9a0f9cc455e4560e2990a461539725e61f783ec6
ponyjackal <pony1125@outlook.com>
2022-02-07 15:55:43 -0500

    update input stories

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { rem } from 'polished';
 import styled from 'styled-components';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Input } from '../components/elements/Input';
@@ -10,11 +11,18 @@ export default {
 } as ComponentMeta<typeof Input>;
 
 const StyledInput = styled(Input)`
-  min-width: 300px;
+  min-width: ${rem(300)};
 `;
 
 const Template: ComponentStory<typeof Input> = (args: any) => {
-  return <StyledInput {...args} />;
+  const [value, setValue] = useState('');
+  return (
+    <StyledInput
+      {...args}
+      value={value}
+      onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
+    />
+  );
 };
 
 export const Default = Template.bind({});
@@ -27,10 +35,15 @@ Disabled.args = {
   disabled: true,
 };
 
-export const Error = Template.bind({});
-Error.args = {
-  label: 'GitHub Repo Url',
-  placeholder: 'github.com/stake-house/wagyu',
-  error: 'Incorrect URL',
-  value: '123456',
+export const Error: ComponentStory<typeof Input> = () => {
+  const [value, setValue] = useState('123456');
+  return (
+    <StyledInput
+      placeholder={'github.com/stake-house/wagyu'}
+      label={'GitHub Repo Url'}
+      value={value}
+      onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
+      error={'Incorrect URL'}
+    />
+  );
 };
