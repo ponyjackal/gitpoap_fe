dece687e121b39e531de7d8f0e4b3956465e2498
ponyjackal <pony1125@outlook.com>
2022-02-22 15:43:28 -0500

    update sample image url

--- src/stories/InfoHex.stories.tsx
+++ src/stories/InfoHex.stories.tsx
@@ -4,6 +4,7 @@ import { InfoHexBase } from '../components/elements/InfoHexBase';
 import { InfoHexMetric } from '../components/InfoHexMetric';
 import { InfoHexProfile } from '../components/InfoHexProfile';
 import { People } from '../components/icons/People';
+import profileImg from './assets/profile1.png';
 
 export default {
   title: 'Elements/InfoHex',
@@ -22,7 +23,7 @@ export const Profile: ComponentStory<typeof InfoHexBase> = (args) => {
   return (
     <InfoHexProfile
       {...args}
-      imgSrc="https://s3-alpha-sig.figma.com/img/69b9/6e34/cfadaaa6702e7f16cfe798e66952c215?Expires=1645401600&Signature=dvsU9Lu793v4-y7XUDDCGDkU0Rrc1nPdlnIQlFpjG8QxzRViwZgKK589hC3nP3kdHN9-xwc5ADiC5BzTAuKkGAiL1hgRR8g8xlKRe8UAhU-u-gs89t7kXgD~5f1k7sHaYKiwrD1WjKM8B62e21pC43y9qQsRnNO8kGq-jviu8NYCErD9BSPp-5u8TQv1Ehx7xUPIltJJ5KMlhY30BEVqReTdM4FQlwA9MaMBEzRSe9rihskuhR3L1lsx38kinASQGqIsha5Pva5amvNFrJkzwvOzPIG-Hmoh0wTyuNiSK9U83zPG6fo~5SKmUonR0vLfZMPAjyZneI36p2i4r0PYLQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+      imgSrc={profileImg as unknown as string}
       name="nd-certora"
       blurb="Recovering physicist turned programmer. Soccer aficionado."
       twitterHref="https://twitter.com/nd_certora"
