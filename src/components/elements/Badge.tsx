149ccf475b949c01f594b2986a4b50e8da30b196
ponyjackal <pony1125@outlook.com>
2022-02-22 15:23:16 -0500

    add text badge

---
+++ src/components/elements/Badge.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { rem } from 'polished';
+import { BackgroundPanel3, TextLight } from '../../colors';
+
+export const Badge = styled.div`
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: ${rem(0.5)} ${rem(4)};
+  background: ${BackgroundPanel3};
+  border-radius: ${rem(4)};
+
+  font-family: PT Mono;
+  font-style: normal;
+  font-weight: bold;
+  font-size: ${rem(11)};
+  line-height: ${rem(18)};
+  letter-spacing: ${rem(1.2)};
+  text-transform: uppercase;
+  color: ${TextLight};
+`;
---
+++ src/stories/Badge.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Badge } from '../components/elements/Badge';
+
+export default {
+  title: 'Elements/Badge',
+  component: Badge,
+} as ComponentMeta<typeof Badge>;
+
+const Template: ComponentStory<typeof Badge> = (args) => {
+  return <Badge {...args}>{'Exchanges'}</Badge>;
+};
+
+export const Default = Template.bind({});
+Default.args = {};
