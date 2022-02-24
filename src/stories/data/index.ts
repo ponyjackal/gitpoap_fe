6a958fb60ee4edb108e7182a15124ce86f16076a
ponyjackal <pony1125@outlook.com>
2022-02-23 19:09:18 -0500

    add shared sample data for storybook

---
+++ src/stories/data/index.ts
@@ -0,0 +1,26 @@
+import { GitPoap } from '../../types';
+import badgeImg from '../assets/badge1.png';
+
+export const poaps: GitPoap[] = [
+  {
+    id: '1',
+    imgSrc: badgeImg as unknown as string,
+    name: 'Polygon Genesis Creator',
+    orgName: 'Polygon',
+    description: 'To the creators of Polygon Network',
+  },
+  {
+    id: '2',
+    imgSrc: badgeImg as unknown as string,
+    name: 'Polygon Genesis Creator',
+    orgName: 'Polygon',
+    description: 'To the creators of Polygon Network',
+  },
+  {
+    id: '3',
+    imgSrc: badgeImg as unknown as string,
+    name: 'Polygon Genesis Creator',
+    orgName: 'Polygon',
+    description: 'To the creators of Polygon Network',
+  },
+];
