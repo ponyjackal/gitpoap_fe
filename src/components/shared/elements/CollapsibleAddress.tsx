d64d7f120199743b0bc0113c60f72aad741f912d
ponyjackal <pony1125@outlook.com>
2022-03-01 16:24:59 -0500

    add collapsible address component

---
+++ src/components/shared/elements/CollapsibleAddress.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import { rem } from 'polished';
+import { useClipboard } from '@mantine/hooks';
+import { FiCopy } from 'react-icons/fi';
+import { Tooltip } from '@mantine/core';
+import { Title } from '../../shared/elements/Title';
+import { TextGray } from '../../../colors';
+
+type Props = {
+  className?: string;
+  address: string;
+  isCollapsed: boolean;
+};
+
+const Address = styled(Title)`
+  font-family: PT Mono;
+  font-style: normal;
+  font-weight: normal;
+  font-size: ${rem(14)};
+  line-height: ${rem(18)};
+  letter-spacing: ${rem(-0.5)};
+  color: ${TextGray};
+`;
+
+export const CollapsibleAddress = ({ className, address, isCollapsed }: Props) => {
+  const clipboard = useClipboard({ timeout: 1500 });
+  const truncatedAddress = address.slice(0, 14) + '...' + address.slice(-4);
+
+  return (
+    <Address className={className} onClick={() => clipboard.copy(address)}>
+      {isCollapsed ? truncatedAddress : address}
+      <Tooltip
+        opened={clipboard.copied}
+        label="Copied!"
+        color="dark"
+        withArrow
+        position="right"
+        placement="end"
+      >
+        <FiCopy style={{ marginLeft: rem(6) }} />
+      </Tooltip>
+    </Address>
+  );
+};
---
+++ src/stories/CollapsibleAddress.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { CollapsibleAddress } from '../components/shared/elements/CollapsibleAddress';
+
+export default {
+  title: 'Elements/CollapsibleAddress',
+  component: CollapsibleAddress,
+} as ComponentMeta<typeof CollapsibleAddress>;
+
+const Template: ComponentStory<typeof CollapsibleAddress> = (args) => {
+  return <CollapsibleAddress {...args}></CollapsibleAddress>;
+};
+
+export const Default = Template.bind({});
+Default.args = { address: '0xe4feb387cb1daff4bf9108581b116e5fa737bea2' };
