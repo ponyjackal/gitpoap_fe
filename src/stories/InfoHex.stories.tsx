85e1a0ebdfce772bb64e1a9dbe52fc01bc7edf67
ponyjackal <pony1125@outlook.com>
2022-03-01 16:27:33 -0500

    add and update stories

--- src/stories/InfoHex.stories.tsx
+++ src/stories/InfoHex.stories.tsx
@@ -3,11 +3,14 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { InfoHexBase } from '../components/shared/elements/InfoHexBase';
 import { InfoHexMetric } from '../components/home/InfoHexMetric';
 import { InfoHexProfile } from '../components/InfoHexProfile';
+import { InfoHexProfileDetail } from '../components/profile/InfoHexProfileDetail';
 import { People } from '../components/shared/elements/icons/People';
 import profileImg from './assets/profile1.png';
+import profileImg6 from './assets/profile6-large.png';
+import { projectData } from './data';
 
 export default {
-  title: 'Elements/InfoHex',
+  title: 'Compounds/InfoHex',
   component: InfoHexBase,
   argTypes: {},
 } as ComponentMeta<typeof InfoHexBase>;
@@ -42,6 +45,23 @@ export const Metrics: ComponentStory<typeof InfoHexMetric> = (args) => {
       unit={'contributors'}
       rate={'+345 / past week '}
       icon={<People />}
+    />
+  );
+};
+
+export const ProfileDetails: ComponentStory<typeof InfoHexBase> = (args) => {
+  return (
+    <InfoHexProfileDetail
+      {...args}
+      imgSrc={profileImg6 as unknown as string}
+      name="miguelmota.eth"
+      address="0xe4feb387cb1daff4bf9108581b116e5fa737bea2"
+      blurb="Passionate for Ethereum, Bitcoin, and client-side development."
+      twitterHref="https://twitter.com/miguelmota"
+      githubHref="github.com/miguelmota"
+      gitpoapId={10}
+      websiteHref="https://miguelmota.com"
+      projects={projectData.slice(0, 3)}
     />
   );
 };
---
+++ src/stories/pages/profile/profile.stories.tsx
@@ -0,0 +1,19 @@
+import Profile from '../../../pages/profile/[id]';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Layout } from '../../../components/Layout';
+
+export default {
+  title: 'Pages/Profile',
+  component: Profile,
+} as ComponentMeta<typeof Profile>;
+
+const Template: ComponentStory<typeof Profile> = () => {
+  return (
+    <Layout>
+      <Profile />
+    </Layout>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {};
