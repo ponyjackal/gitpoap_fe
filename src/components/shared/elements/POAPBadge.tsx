20745cdf63ef1a195f15bdfc84ff93a455d83e51
ponyjackal <pony1125@outlook.com>
2022-03-02 15:46:54 -0500

    perfect the poap badge

--- src/components/shared/elements/POAPBadge.tsx
+++ src/components/shared/elements/POAPBadge.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 import { rem } from 'polished';
+import Image from 'next/image';
 import { Title as TitleUI } from '../elements/Title';
 import { TextLight } from '../../../colors';
 
 type Props = {
+  className?: string;
   imgSrc: string;
   name: string;
   href?: string;
 };
 
-const Badge = styled.div<{ imgSrc: string }>`
+const Container = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ImgContainer = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const POAP = styled(Image)`
   width: ${rem(150)};
   height: ${rem(150)};
   border-radius: 50%;
-  background: url('${(props) => props.imgSrc}') no-repeat center center, #ffffff;
-  background-size: cover;
+  background: #ffffff;
   transition: 150ms background-color ease-in-out, 150ms opacity ease-in-out;
 
   &:hover:not([disabled]) {
@@ -30,13 +44,16 @@ const Badge = styled.div<{ imgSrc: string }>`
 const Title = styled(TitleUI)`
   margin-top: ${rem(18)};
   color: ${TextLight};
+  width: ${rem(170)};
 `;
 
-export const POAPBadge = ({ imgSrc, name, href }: Props) => {
+export const POAPBadge = ({ className, imgSrc, name, href }: Props) => {
   return (
-    <>
-      <Badge imgSrc={imgSrc} />
+    <Container className={className}>
+      <ImgContainer>
+        <POAP quality={100} height={150} width={150} src={imgSrc} />
+      </ImgContainer>
       <Title>{name}</Title>
-    </>
+    </Container>
   );
 };
