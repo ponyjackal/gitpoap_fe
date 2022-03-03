44fb390277cbd7cf849d074d1244b6cca3cd9a1d
ponyjackal <pony1125@outlook.com>
2022-03-03 17:25:07 -0500

    move all providers to withProviders and create mock Next Router

--- .storybook/decorators/withGlobalStyles.tsx
+++ .storybook/decorators/withGlobalStyles.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { GlobalStyles } from '../../src/styles/globalStyles';
-import { MantineProvider } from '@mantine/core';
 
 export const withGlobalStyles = (storyFn: any) => {
   return (
     <>
-      <MantineProvider theme={{ colorScheme: 'dark' }}>
-        <GlobalStyles />
-        {storyFn()}
-      </MantineProvider>
+      <GlobalStyles />
+      {storyFn()}
     </>
   );
 };
--- .storybook/decorators/withProviders.tsx
+++ .storybook/decorators/withProviders.tsx
@@ -2,18 +2,49 @@ import React from 'react';
 import { Web3ContextProvider } from '../../src/components/wallet/Web3ContextProvider';
 import { GHAuthProvider } from '../../src/components/github/GHAuthContext';
 import { createClient, Provider as URQLProvider } from 'urql';
+import { MantineProvider } from '@mantine/core';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+import { NextRouter } from 'next/router';
 
 const client = createClient({
   url: 'http://localhost:3001/graphql',
   requestPolicy: 'network-only',
 });
 
+const mockRouter: NextRouter = {
+  basePath: '',
+  pathname: '/',
+  route: '/',
+  asPath: '/',
+  query: {},
+  /* @ts-ignore */
+  push: () => {},
+  /* @ts-ignore */
+  replace: () => {},
+  reload: () => {},
+  back: () => {},
+  /* @ts-ignore */
+  prefetch: () => {},
+  beforePopState: () => {},
+  events: {
+    on: () => {},
+    off: () => {},
+    emit: () => {},
+  },
+  isFallback: false,
+  isLocaleDomain: false,
+};
+
 export const withProviders = (storyFn) => {
   return (
-    <URQLProvider value={client}>
+    <RouterContext.Provider value={mockRouter}>
       <Web3ContextProvider>
-        <GHAuthProvider>{storyFn()}</GHAuthProvider>
+        <MantineProvider theme={{ colorScheme: 'dark' }}>
+          <URQLProvider value={client}>
+            <GHAuthProvider>{storyFn()}</GHAuthProvider>
+          </URQLProvider>
+        </MantineProvider>
       </Web3ContextProvider>
-    </URQLProvider>
+    </RouterContext.Provider>
   );
 };
