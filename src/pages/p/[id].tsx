cad5bf188075eff9c17eec82b28016982fc97ba9
ponyjackal <pony1125@outlook.com>
2022-03-10 15:22:29 -0500

    fetch ens name and address in profile top level component

--- src/pages/p/[id].tsx
+++ src/pages/p/[id].tsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { rem } from 'polished';
 import Head from 'next/head';
 import { Grid } from '@mantine/core';
+import { isAddress } from 'ethers/lib/utils';
 import { Page } from '../_app';
 import { Layout } from '../../components/Layout';
 import { AllPOAPs } from '../../components/profile/AllPOAPs';
 import { GitPOAPs } from '../../components/profile/GitPOAPs';
 import { useRouter } from 'next/router';
+import { ProfileSidebar } from '../../components/profile/ProfileSidebar';
+import { useWeb3Context } from '../../components/wallet/Web3ContextProvider';
 
 const Profile: Page = () => {
   const router = useRouter();
-  /* ENS or ETH address */
-  const address = router.query.id as string;
+  const [address, setAddress] = useState<string>('');
+  const [ensName, setEnsName] = useState<string>('');
+  const { web3Provider } = useWeb3Context();
 
-  if (!router.isReady) {
+  const nameOrAddress = router.query.id as string;
+
+  /* This hook is used to set the name */
+  useEffect(() => {
+    const lookupName = async () => {
+      if (isAddress(nameOrAddress)) {
+        const address = nameOrAddress;
+        setAddress(address);
+        const ensName = await web3Provider?.lookupAddress(address);
+
+        if (ensName) {
+          setEnsName(ensName);
+        }
+      } else if (nameOrAddress.includes('.eth')) {
+        const name = nameOrAddress;
+        const address = await web3Provider?.resolveName(name);
+
+        if (address) {
+          setEnsName(name);
+          setAddress(address);
+        }
+      }
+    };
+
+    if (router.isReady) {
+      lookupName();
+    }
+  }, [nameOrAddress, web3Provider, router]);
+
+  if (!(router.isReady && address)) {
     return null;
   }
 
@@ -26,14 +59,14 @@ const Profile: Page = () => {
       </Head>
       <div style={{ color: 'white' }}>{'The Banner goes here'}</div>
       <Grid style={{ color: 'white' }} justify="center">
-        <Grid.Col span={12}>{'The Profile Hex'}</Grid.Col>
+        <ProfileSidebar address={address} ensName={ensName} />
         <Grid justify="center">
           <Grid.Col span={11}>{'Featured POAPs'}</Grid.Col>
           <Grid.Col span={11}>
-            <GitPOAPs address={address} />
+            <GitPOAPs address={nameOrAddress} />
           </Grid.Col>
           <Grid.Col span={11} style={{ marginBottom: rem(150) }}>
-            <AllPOAPs address={address} />
+            <AllPOAPs address={nameOrAddress} />
           </Grid.Col>
         </Grid>
       </Grid>
