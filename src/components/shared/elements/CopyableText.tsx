a1d97e75d157fc9b126b154ca010f257526c7fd0
ponyjackal <pony1125@outlook.com>
2022-03-10 13:42:10 -0500

    Create CopyableText componnet

---
+++ src/components/shared/elements/CopyableText.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import { rem } from 'polished';
+import { useClipboard } from '@mantine/hooks';
+import { FiCopy } from 'react-icons/fi';
+import { Tooltip } from '@mantine/core';
+import { Title } from './Title';
+import { TextGray } from '../../../colors';
+
+type Props = {
+  className?: string;
+  text: string;
+  textToCopy: string;
+};
+
+const CopyableTextStyled = styled(Title)`
+  font-family: PT Mono;
+  font-style: normal;
+  font-weight: normal;
+  font-size: ${rem(14)};
+  line-height: ${rem(18)};
+  letter-spacing: ${rem(-0.5)};
+  color: ${TextGray};
+`;
+
+export const CopyableText = ({ className, text, textToCopy }: Props) => {
+  const clipboard = useClipboard({ timeout: 1500 });
+
+  return (
+    <CopyableTextStyled className={className} onClick={() => clipboard.copy(textToCopy)}>
+      {text}
+      <Tooltip opened={clipboard.copied} label="Copied!" withArrow position="right" placement="end">
+        <FiCopy style={{ marginLeft: rem(6) }} />
+      </Tooltip>
+    </CopyableTextStyled>
+  );
+};
---
+++ src/stories/CopyableText.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { CopyableText } from '../components/shared/elements/CopyableText';
+
+export default {
+  title: 'Elements/CopyableText',
+  component: CopyableText,
+} as ComponentMeta<typeof CopyableText>;
+
+const Template: ComponentStory<typeof CopyableText> = (args) => {
+  return <CopyableText {...args}></CopyableText>;
+};
+
+export const Default = Template.bind({});
+Default.args = { text: 'URL: gitpoap.io/p/burz.eth', textToCopy: 'https://gitpoap.io/p/burz.eth' };
