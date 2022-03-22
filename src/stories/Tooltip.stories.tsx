5179cfedeb72bd92412e30d3bf2ecf71fc754992
ponyjackal <pony1125@outlook.com>
2022-03-22 15:46:22 -0400

    Add a story for the Tooltip element

---
+++ src/stories/Tooltip.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Tooltip } from '../components/shared/elements/Tooltip';
+import { Text } from '../components/shared/elements/Text';
+
+export default {
+  title: 'Elements/Tooltip',
+  component: Tooltip,
+} as ComponentMeta<typeof Tooltip>;
+
+const Template: ComponentStory<typeof Tooltip> = (args) => {
+  return (
+    <Tooltip {...args}>
+      <Text>{'Exchanges'}</Text>
+    </Tooltip>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = { label: 'An exchange is a place where things are exchanged', withArrow: true };
