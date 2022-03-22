493c1481dec402112ba1a1b5c85f44b8499bc5ec
ponyjackal <pony1125@outlook.com>
2022-03-22 14:48:42 -0400

    don't show featured poaps unless there is one

--- src/components/profile/FeaturedPOAPs.tsx
+++ src/components/profile/FeaturedPOAPs.tsx
@@ -6,6 +6,7 @@ import { POAPBadge as POAPBadgeUI } from '../shared/elements/POAPBadge';
 import { TextAccent } from '../../colors';
 import { GitPOAP } from '../shared/compounds/GitPOAP';
 import { useFeaturedPOAPs, GitPOAP as GitPOAPType } from './FeaturedPOAPsContext';
+import { Text } from '@mantine/core';
 
 const POAPs = styled.div`
   display: inline-flex;
@@ -21,16 +22,15 @@ const POAPBadge = styled(POAPBadgeUI)`
   margin-top: ${rem(30)};
 `;
 
-const SectionTitle = styled.div`
+const SectionTitle = styled(Text)`
   font-family: VT323;
   font-style: normal;
   font-weight: normal;
-  font-size: ${rem(36)};
+  font-size: ${rem(30)};
   line-height: ${rem(42)};
-  text-align: center;
   letter-spacing: ${rem(-1)};
   color: ${TextAccent};
-  margin-bottom: ${rem(4)};
+  margin-bottom: ${rem(30)};
 `;
 
 const isGitPOAP = (poap: POAP | GitPOAPType): poap is GitPOAPType => {
@@ -44,34 +44,37 @@ export const FeaturedPOAPs = () => {
 
   return (
     <>
-      <SectionTitle>{'Featured by the user'}</SectionTitle>
-      <POAPs>
-        {featuredPOAPsFull &&
-          featuredPOAPsFull.map((featuredPOAP) => {
-            if (isGitPOAP(featuredPOAP)) {
+      {featuredPOAPsFull.length > 0 && (
+        <>
+          <SectionTitle>{'Featured by the user'}</SectionTitle>
+          <POAPs>
+            {featuredPOAPsFull.map((featuredPOAP) => {
+              if (isGitPOAP(featuredPOAP)) {
+                return (
+                  <GitPOAP
+                    key={featuredPOAP.claim.id}
+                    name={featuredPOAP.poap.event.name}
+                    imgSrc={featuredPOAP.poap.event.image_url}
+                    orgName={featuredPOAP.claim.gitPOAP.repo.organization.name}
+                    description={featuredPOAP.poap.event.description}
+                    poapTokenId={featuredPOAP.poap.tokenId}
+                  />
+                );
+              }
+
               return (
-                <GitPOAP
-                  key={featuredPOAP.claim.id}
-                  name={featuredPOAP.poap.event.name}
-                  imgSrc={featuredPOAP.poap.event.image_url}
-                  orgName={featuredPOAP.claim.gitPOAP.repo.organization.name}
-                  description={featuredPOAP.poap.event.description}
-                  poapTokenId={featuredPOAP.poap.tokenId}
+                <POAPBadge
+                  key={featuredPOAP.tokenId}
+                  poapTokenId={featuredPOAP.tokenId}
+                  name={featuredPOAP.event.name}
+                  imgSrc={featuredPOAP.event.image_url}
+                  href={`https://app.poap.xyz/token/${featuredPOAP.tokenId}`}
                 />
               );
-            }
-
-            return (
-              <POAPBadge
-                key={featuredPOAP.tokenId}
-                poapTokenId={featuredPOAP.tokenId}
-                name={featuredPOAP.event.name}
-                imgSrc={featuredPOAP.event.image_url}
-                href={`https://app.poap.xyz/token/${featuredPOAP.tokenId}`}
-              />
-            );
-          })}
-      </POAPs>
+            })}
+          </POAPs>
+        </>
+      )}
     </>
   );
 };
