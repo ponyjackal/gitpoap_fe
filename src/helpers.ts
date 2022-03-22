40499415453b9d4efe6ed2f1f9c29a79da2f1222
ponyjackal <pony1125@outlook.com>
2022-03-22 14:48:14 -0400

    remove error that should not be thrown..

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,10 +1,5 @@
-import { isAddress } from 'ethers/lib/utils';
-
 /* Shorten check-summed version of the input address ~ 0x + 4 chars @ start + end */
 export function shortenAddress(address: string, chars = 4): string {
-  if (!isAddress(address)) {
-    throw Error(`Invalid 'address' parameter '${address}'.`);
-  }
   return `${address.substring(0, chars + 2)}...${address.substring(42 - chars)}`;
 }
 
