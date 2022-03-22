d62d843d128bdbcce3b399bf13363559964f2f35
ponyjackal <pony1125@outlook.com>
2022-03-22 16:21:43 -0400

    fix app content not filling screen issue

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,33 @@
+import styled from 'styled-components';
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
+import { MidnightBlue } from '../colors';
 
 type Props = {
   children: React.ReactNode;
 };
 
+const App = styled.div`
+  background-color: ${MidnightBlue};
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  font-family: 'PT Mono', monospace;
+  min-height: 100vh;
+  min-width: 300px;
+  overflow-x: hidden;
+`;
+
+const MainContent = styled.main`
+  flex: 1;
+`;
+
 export const Layout = ({ children }: Props) => {
   return (
-    <>
+    <App>
       <Navbar />
-      <main>{children}</main>
+      <MainContent>{children}</MainContent>
       <Footer />
-    </>
+    </App>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-import styled from 'styled-components';
 import { createClient, Provider as URQLProvider } from 'urql';
 import { MantineProvider } from '@mantine/core';
 import { GlobalStyles } from '../styles/globalStyles';
@@ -8,7 +7,6 @@ import { AuthProvider } from '../components/github/AuthContext';
 import { FeaturesProvider } from '../components/FeaturesContext';
 import { NextPage } from 'next';
 import { Layout } from '../components/Layout';
-import { MidnightBlue } from '../colors';
 
 const client = createClient({
   url: 'http://localhost:3001/graphql',
@@ -22,17 +20,6 @@ type Props = AppProps & {
   Component: Page;
 };
 
-const App = styled.div`
-  background-color: ${MidnightBlue};
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  font-family: 'PT Mono', monospace;
-  min-height: 100vh;
-  min-width: 300px;
-  overflow-x: hidden;
-`;
-
 const TheApp = ({ Component, pageProps }: Props) => {
   /* Use custom page-specific layout once / if needed */
   const getLayout = Component.getLayout || ((page: React.ReactNode) => page);
@@ -44,11 +31,9 @@ const TheApp = ({ Component, pageProps }: Props) => {
           <AuthProvider>
             <FeaturesProvider>
               <GlobalStyles />
-              <App>
-                <Layout>
-                  <Component {...pageProps} />
-                </Layout>
-              </App>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
             </FeaturesProvider>
           </AuthProvider>
         </URQLProvider>
