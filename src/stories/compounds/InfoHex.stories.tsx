10b0e1203cd3e091408dbb3edf989e7a16314c0f
ponyjackal <pony1125@outlook.com>
2022-03-23 16:16:30 -0400

    split out info hex profile stories

--- src/stories/compounds/InfoHex.stories.tsx
+++ src/stories/compounds/InfoHex.stories.tsx
@@ -3,11 +3,8 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { InfoHexBase } from '../../components/shared/elements/InfoHexBase';
 import { InfoHexMetric } from '../../components/home/InfoHexMetric';
 import { InfoHexSummary } from '../../components/gitpoap/InfoHexSummary';
-import { InfoHexProfileDetail } from '../../components/profile/InfoHexProfileDetail';
 import { People } from '../../components/shared/elements/icons/People';
 import profileImg from '../assets/profile1.png';
-import profileImg6 from '../assets/profile6-large.png';
-import { projectData } from '../data';
 
 export default {
   title: 'Compounds/InfoHex',
@@ -45,24 +42,6 @@ export const Profile: ComponentStory<typeof InfoHexBase> = (args) => {
       githubHref="github.com/nd_certora"
       gitpoapId={10}
       numGitPOAPs={12}
-    />
-  );
-};
-
-export const ProfileDetails: ComponentStory<typeof InfoHexBase> = (args) => {
-  return (
-    <InfoHexProfileDetail
-      {...args}
-      imgSrc={profileImg6 as unknown as string}
-      name="miguelmota.eth"
-      address="0xe4feb387cb1daff4bf9108581b116e5fa737bea2"
-      bio="Passionate for Ethereum, Bitcoin, and client-side development."
-      twitterHref="https://twitter.com/miguelmota"
-      githubHref="github.com/miguelmota"
-      websiteHref="https://miguelmota.com"
-      projects={projectData.slice(0, 3)}
-      onClickEditProfile={() => {}}
-      showEditProfileButton={true}
     />
   );
 };
---
+++ src/stories/compounds/InfoHexProfile.stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { InfoHexBase } from '../../components/shared/elements/InfoHexBase';
+import { InfoHexProfileDetail } from '../../components/profile/InfoHexProfileDetail';
+import profileImg6 from '../assets/profile6-large.png';
+import { projectData } from '../data';
+
+export default {
+  title: 'Compounds/InfoHexProfile',
+  component: InfoHexBase,
+  argTypes: {},
+} as ComponentMeta<typeof InfoHexBase>;
+
+export const WithProjects: ComponentStory<typeof InfoHexBase> = (args) => {
+  return (
+    <InfoHexProfileDetail
+      {...args}
+      imgSrc={profileImg6 as unknown as string}
+      name="miguelmota.eth"
+      address="0xe4feb387cb1daff4bf9108581b116e5fa737bea2"
+      ensName="miguelmota.eth"
+      bio="Passionate for Ethereum, Bitcoin, and client-side development."
+      twitterHref="https://twitter.com/miguelmota"
+      githubHref="github.com/miguelmota"
+      websiteHref="https://miguelmota.com"
+      projects={projectData.slice(0, 3)}
+      onClickEditProfile={() => console.warn('onClickEditProfile')}
+      showEditProfileButton={true}
+    />
+  );
+};
+
+export const WithoutProjects: ComponentStory<typeof InfoHexBase> = (args) => {
+  return (
+    <InfoHexProfileDetail
+      {...args}
+      imgSrc={profileImg6 as unknown as string}
+      name="miguelmota.eth"
+      address="0xe4feb387cb1daff4bf9108581b116e5fa737bea2"
+      ensName="miguelmota.eth"
+      bio="Passionate for Ethereum, Bitcoin, and client-side development."
+      twitterHref="https://twitter.com/miguelmota"
+      githubHref="github.com/miguelmota"
+      websiteHref="https://miguelmota.com"
+      onClickEditProfile={() => console.warn('onClickEditProfile')}
+      showEditProfileButton={true}
+    />
+  );
+};
