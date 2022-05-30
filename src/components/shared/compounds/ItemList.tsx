c7e5e868820cc1e79f5ac3993937ef149defab89
ponyjackal <pony1125@outlook.com>
2022-05-29 21:28:23 -0400

    add classname to item list

--- src/components/shared/compounds/ItemList.tsx
+++ src/components/shared/compounds/ItemList.tsx
@@ -11,6 +11,7 @@ import { Input } from '../elements/Input';
 
 type Props = {
   title: string;
+  className?: string;
   children?: React.ReactNode;
   selectOptions: SelectOption[];
   selectValue: string;
@@ -70,6 +71,7 @@ const SearchInput = styled(Input)`
 
 export const ItemList = ({
   children,
+  className,
   title,
   selectOptions,
   selectValue,
@@ -82,7 +84,7 @@ export const ItemList = ({
   onSearchInputChange,
 }: Props) => {
   return (
-    <Container>
+    <Container className={className}>
       <Heading>
         <ListTitle>{title}</ListTitle>
         <Sorting>
