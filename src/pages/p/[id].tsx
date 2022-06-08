1319597cfe8f7f09ef0f97a4fe4d40a8e9ebc011
ponyjackal <pony1125@outlook.com>
2022-06-08 11:51:33 -0400

    vastly simplify the way we fetch ensNames for the profile page

--- src/pages/p/[id].tsx
+++ src/pages/p/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { rem } from 'polished';
 import { Grid } from '@mantine/core';
@@ -8,10 +8,8 @@ import { Page } from '../_app';
 import { Layout } from '../../components/Layout';
 import { AllPOAPs } from '../../components/profile/AllPOAPs';
 import { GitPOAPs } from '../../components/profile/GitPOAPs';
-import { useRouter } from 'next/router';
 import { ProfileSidebar } from '../../components/profile/ProfileSidebar';
 import { FeaturedPOAPs } from '../../components/profile/FeaturedPOAPs';
-import { useWeb3Context } from '../../components/wallet/Web3ContextProvider';
 import { FeaturedPOAPsProvider } from '../../components/profile/FeaturedPOAPsContext';
 import { ProfileProvider } from '../../components/profile/ProfileContext';
 import { truncateAddress } from '../../helpers';
@@ -40,95 +38,57 @@ const ProfileSidebarWrapper = styled(Grid.Col)`
 `;
 
 type PageProps = {
-  nameOrAddress: string;
+  addressOrEns: string;
 };
 
 const Profile: Page<PageProps> = (props) => {
-  const router = useRouter();
-  const [profileAddress, setProfileAddress] = useState<string | null>(null);
-  const [ensName, setEnsName] = useState<string | null>(null);
-  const { web3Provider, infuraProvider } = useWeb3Context();
-  const nameOrAddress = router.query.id as string;
-
-  /* This hook is used to set the address and/or ENS name resolved from the URL */
-  useEffect(() => {
-    const lookupName = async () => {
-      if (isAddress(nameOrAddress)) {
-        const profileAddress = nameOrAddress;
-        setProfileAddress(profileAddress);
-        const ensName = await infuraProvider?.lookupAddress(profileAddress);
-
-        if (ensName) {
-          setEnsName(ensName);
-        } else {
-          setEnsName(null);
-        }
-      } else if (nameOrAddress.includes('.eth')) {
-        const name = nameOrAddress;
-        const profileAddress = await infuraProvider?.resolveName(name);
-
-        if (profileAddress) {
-          setProfileAddress(profileAddress);
-        }
-        setEnsName(name);
-      }
-    };
-
-    if (router.isReady) {
-      lookupName();
-    }
-  }, [nameOrAddress, web3Provider, router, infuraProvider]);
-
   return (
     <>
       <SEO
         title={`${
-          isAddress(props.nameOrAddress)
-            ? truncateAddress(props.nameOrAddress ?? '', 4)
-            : props.nameOrAddress
+          isAddress(props.addressOrEns)
+            ? truncateAddress(props.addressOrEns ?? '', 4)
+            : props.addressOrEns
         } | GitPOAP`}
         description={
           'GitPOAP is a decentralized reputation platform that represents off-chain accomplishments and contributions on chain as POAPs.'
         }
         image={'https://gitpoap.io/og-image-512x512.png'}
-        url={`https://gitpoap.io/p/${props.nameOrAddress}`}
+        url={`https://gitpoap.io/p/${props.addressOrEns}`}
       />
-      {router.isReady && (!!profileAddress || !!ensName) && (
-        <ProfileProvider address={profileAddress} ensName={ensName}>
-          <FeaturedPOAPsProvider profileAddress={profileAddress} ensName={ensName}>
-            <Grid justify="center" style={{ marginTop: rem(40), zIndex: 1 }}>
-              <Background />
-              <ProfileSidebarWrapper lg={2}>
-                <ProfileSidebar address={profileAddress} ensName={ensName} />
-              </ProfileSidebarWrapper>
-
-              <Grid.Col lg={8} style={{ zIndex: 1 }}>
-                <Grid justify="center">
-                  <Grid.Col span={10} style={{ marginTop: rem(60) }}>
-                    <FeaturedPOAPs />
-                  </Grid.Col>
-                  <Grid.Col span={10}>
-                    <GitPOAPs address={nameOrAddress} />
-                  </Grid.Col>
-                  <Grid.Col span={10} style={{ marginBottom: rem(150) }}>
-                    <AllPOAPs address={nameOrAddress} />
-                  </Grid.Col>
-                </Grid>
-              </Grid.Col>
-            </Grid>
-          </FeaturedPOAPsProvider>
-        </ProfileProvider>
-      )}
+      <ProfileProvider addressOrEns={props.addressOrEns}>
+        <FeaturedPOAPsProvider>
+          <Grid justify="center" style={{ marginTop: rem(40), zIndex: 1 }}>
+            <Background />
+            <ProfileSidebarWrapper lg={2}>
+              <ProfileSidebar />
+            </ProfileSidebarWrapper>
+            <Grid.Col lg={8} style={{ zIndex: 1 }}>
+              <Grid justify="center">
+                <Grid.Col span={10} style={{ marginTop: rem(60) }}>
+                  <FeaturedPOAPs />
+                </Grid.Col>
+                <Grid.Col span={10}>
+                  <GitPOAPs address={props.addressOrEns} />
+                </Grid.Col>
+                <Grid.Col span={10} style={{ marginBottom: rem(150) }}>
+                  <AllPOAPs address={props.addressOrEns} />
+                </Grid.Col>
+              </Grid>
+            </Grid.Col>
+          </Grid>
+        </FeaturedPOAPsProvider>
+      </ProfileProvider>
     </>
   );
 };
 
-export async function getServerSideProps(context: NextPageContext) {
-  const nameOrAddress = context.query.id as string;
+export async function getServerSideProps(context: NextPageContext): Promise<{ props: PageProps }> {
+  const addressOrEns = context.query.id as string;
 
   return {
     props: {
-      nameOrAddress,
+      addressOrEns,
     },
   };
 }
