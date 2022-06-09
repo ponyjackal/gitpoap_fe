d8e4bce26f2ec9400c765f730fd1de7f651e1edb
ponyjackal <pony1125@outlook.com>
2022-06-09 15:53:33 -0400

    use the new component

--- src/pages/admin/gitpoap/claims.tsx
+++ src/pages/admin/gitpoap/claims.tsx
@@ -2,45 +2,52 @@ import React from 'react';
 import { rem } from 'polished';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Grid, Group, Table as TableUI } from '@mantine/core';
-import { Header, Text } from '../../../components/shared/elements';
-import { Divider } from '../../../components/shared/elements';
+import { Grid } from '@mantine/core';
 import { useAuthContext } from '../../../components/github/AuthContext';
 import { ConnectGitHub } from '../../../components/admin/ConnectGitHub';
 import { useAdminClaimsQuery, useGetAllStatsQuery } from '../../../graphql/generated-gql';
 import { DateTime } from 'luxon';
 import { truncateAddress } from '../../../helpers';
-import styled from 'styled-components';
-import { TextLight } from '../../../colors';
+import TableDashboard from '../../../components/admin/TableDashboard';
 
-const ClaimRowItem = (props: { children: React.ReactNode }) => {
-  return (
-    <td>
-      <Text>{props.children}</Text>
-    </td>
-  );
+type RowData = {
+  'Claim ID': number;
+  'Github User': string;
+  'User ID': number;
+  Repo: string;
+  Status: string;
+  'Poap Token ID': string;
+  Address: string;
+  'Claimed At': string;
+  'Created At': string;
 };
-
-const Table = styled(TableUI)`
-  thead th {
-    font-family: PT Mono;
-    font-style: normal;
-    font-weight: normal;
-    font-size: ${rem(14)};
-    line-height: ${rem(20)};
-    letter-spacing: ${rem(0.2)};
-    color: ${TextLight} !important;
-  }
-`;
 
 const ClaimsDashboard: NextPage = () => {
   const { isLoggedIntoGitHub } = useAuthContext();
   const [result] = useAdminClaimsQuery({
     variables: {
-      count: 20,
+      count: 200,
     },
   });
   const [resultStats] = useGetAllStatsQuery();
+
+  const topRowData = {
+    'Total Completed Claims': resultStats.data?.totalClaims ?? '',
+  };
+
+  const data = result.data?.claims.map((claim) => {
+    return {
+      'Claim ID': claim.id,
+      'Github User': claim.user.githubHandle,
+      'User ID': claim.user.id,
+      Repo: claim.gitPOAP.repo.name,
+      Status: claim.status,
+      'Poap Token ID': claim.poapTokenId ?? '',
+      Address: truncateAddress(claim.address ?? '', 6),
+      'Claimed At': DateTime.fromISO(claim.claimedAt).toFormat('dd LLL yyyy hh:mm'),
+      'Created At': DateTime.fromISO(claim.createdAt).toFormat('dd LLL yyyy hh:mm'),
+    };
+  });
 
   return (
     <div>
@@ -51,55 +58,15 @@ const ClaimsDashboard: NextPage = () => {
       <Grid justify="center" style={{ marginTop: rem(20) }}>
         <Grid.Col xs={10} sm={10} md={10} lg={10} xl={10}>
           {isLoggedIntoGitHub ? (
-            <Group direction="row" position="center">
-              <Group direction="column">
-                <Group position="apart" align="flex-end" style={{ width: '100%' }}>
-                  <Header>{'Admin - Claims Dashboard'}</Header>
-                  <Text
-                    style={{ verticalAlign: 'text-bottom' }}
-                  >{`Total Completed Claims: ${resultStats.data?.totalClaims}`}</Text>
-                </Group>
-                <Divider style={{ width: '100%', marginTop: rem(10), marginBottom: rem(10) }} />
-                <Table>
-                  <thead>
-                    <tr>
-                      <th>{'Claim ID'}</th>
-                      <th>{'Github User'}</th>
-                      <th>{'User ID'}</th>
-                      <th>{'Repo'}</th>
-                      <th>{'Status'}</th>
-                      <th>{'Poap Token ID'}</th>
-                      <th>{'Address'}</th>
-                      <th>{'Claimed At'}</th>
-                      <th>{'Created At'}</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {result.data?.claims.map((claim) => (
-                      <tr key={claim.id}>
-                        <ClaimRowItem key="id">
-                          <Text>{claim.id}</Text>
-                        </ClaimRowItem>
-                        <ClaimRowItem key="githubUser">{claim.user.githubHandle}</ClaimRowItem>
-                        <ClaimRowItem key="githubUser">{claim.user.id}</ClaimRowItem>
-                        <ClaimRowItem key="repoName">{claim.gitPOAP.repo.name}</ClaimRowItem>
-                        <ClaimRowItem key="status">{claim.status}</ClaimRowItem>
-                        <ClaimRowItem key="poapTokenId">{claim.poapTokenId}</ClaimRowItem>
-                        <ClaimRowItem key="address">
-                          {truncateAddress(claim.address ?? '', 6)}
-                        </ClaimRowItem>
-                        <ClaimRowItem key="claimedAt">
-                          {DateTime.fromISO(claim.claimedAt).toFormat('dd LLL yyyy hh:mm')}
-                        </ClaimRowItem>
-                        <ClaimRowItem key="createdAt">
-                          {DateTime.fromISO(claim.createdAt).toFormat('dd LLL yyyy hh:mm')}
-                        </ClaimRowItem>
-                      </tr>
-                    ))}
-                  </tbody>
-                </Table>
-              </Group>
-            </Group>
+            <>
+              {data && (
+                <TableDashboard<RowData[]>
+                  name="Admin - Repos Dashboard"
+                  data={data}
+                  topRowData={topRowData}
+                />
+              )}
+            </>
           ) : (
             <ConnectGitHub />
           )}
--- src/pages/admin/repos.tsx
+++ src/pages/admin/repos.tsx
@@ -1,45 +1,44 @@
 import React from 'react';
-import styled from 'styled-components';
 import { rem } from 'polished';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Grid, Group, Table as TableUI } from '@mantine/core';
+import { Grid } from '@mantine/core';
 import { DateTime } from 'luxon';
-import { Header, Text } from '../../components/shared/elements';
-import { Divider } from '../../components/shared/elements';
 import { useAuthContext } from '../../components/github/AuthContext';
 import { ConnectGitHub } from '../../components/admin/ConnectGitHub';
 import { useAllReposQuery, useGetAllStatsQuery } from '../../graphql/generated-gql';
-import { TextLight } from '../../colors';
+import TableDashboard from '../../components/admin/TableDashboard';
 
-const RepoRowItem = (props: { children: React.ReactNode }) => {
-  return (
-    <td>
-      <Text>{props.children}</Text>
-    </td>
-  );
+type RowData = {
+  'Repo ID': number;
+  Name: string;
+  Organization: string;
+  GitPOAPs: number;
+  'Created At': string;
 };
-
-const Table = styled(TableUI)`
-  thead th {
-    font-family: PT Mono;
-    font-style: normal;
-    font-weight: normal;
-    font-size: ${rem(14)};
-    line-height: ${rem(20)};
-    letter-spacing: ${rem(0.2)};
-    color: ${TextLight} !important;
-  }
-`;
 
 const ReposDashboard: NextPage = () => {
   const { isLoggedIntoGitHub } = useAuthContext();
   const [result] = useAllReposQuery({
     variables: {
-      count: 20,
+      count: 200,
     },
   });
   const [resultStats] = useGetAllStatsQuery();
+
+  const topRowData = {
+    'Total Added Repos': resultStats.data?.totalRepos ?? '',
+  };
+
+  const data = result.data?.repos.map((repo) => {
+    return {
+      'Repo ID': repo.id,
+      Name: repo.name,
+      Organization: repo.organization.name,
+      GitPOAPs: repo.gitPOAPs.length,
+      'Created At': DateTime.fromISO(repo.createdAt).toFormat('dd LLL yyyy hh:mm'),
+    };
+  });
 
   return (
     <div>
@@ -50,43 +49,15 @@ const ReposDashboard: NextPage = () => {
       <Grid justify="center" style={{ marginTop: rem(20) }}>
         <Grid.Col xs={10} sm={10} md={10} lg={10} xl={10}>
           {isLoggedIntoGitHub ? (
-            <Group direction="row" position="center">
-              <Group direction="column">
-                <Group position="apart" align="flex-end" style={{ width: '100%' }}>
-                  <Header>{'Admin - Repos Dashboard'}</Header>
-                  <Text
-                    style={{ verticalAlign: 'text-bottom' }}
-                  >{`Total Added Repos: ${resultStats.data?.totalRepos}`}</Text>
-                </Group>
-                <Divider style={{ width: '100%', marginTop: rem(10), marginBottom: rem(10) }} />
-                <Table>
-                  <thead>
-                    <tr>
-                      <th>{'Repo ID'}</th>
-                      <th>{'Name'}</th>
-                      <th>{'Organization'}</th>
-                      <th>{'GitPOAPs'}</th>
-                      <th>{'Created At'}</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {result.data?.repos.map((repo) => (
-                      <tr key={repo.id}>
-                        <RepoRowItem key="id">
-                          <Text>{repo.id}</Text>
-                        </RepoRowItem>
-                        <RepoRowItem key="repoName">{repo.name}</RepoRowItem>
-                        <RepoRowItem key="organizationName">{repo.organization.name}</RepoRowItem>
-                        <RepoRowItem key="gitPOAPCount">{repo.gitPOAPs.length}</RepoRowItem>
-                        <RepoRowItem key="createdAt">
-                          {DateTime.fromISO(repo.createdAt).toFormat('dd LLL yyyy hh:mm')}
-                        </RepoRowItem>
-                      </tr>
-                    ))}
-                  </tbody>
-                </Table>
-              </Group>
-            </Group>
+            <>
+              {data && (
+                <TableDashboard<RowData[]>
+                  name="Admin - Repos Dashboard"
+                  data={data}
+                  topRowData={topRowData}
+                />
+              )}
+            </>
           ) : (
             <ConnectGitHub />
           )}
