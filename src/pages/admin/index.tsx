7c9f59314e4bed303b3ee70615cf222a61916ed2
ponyjackal <pony1125@outlook.com>
2022-06-28 18:27:07 -0400

    use re-usable Link on admin home page

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { rem } from 'polished';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Link from 'next/link';
+import { Link } from '../../components/Link';
 import { Grid, Group } from '@mantine/core';
 import { Header } from '../../components/shared/elements';
 import { ExtraHover, ExtraPressed } from '../../colors';
