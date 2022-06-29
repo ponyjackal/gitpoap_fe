32d31952f7d57291d8424003416d827048147a64
ponyjackal <pony1125@outlook.com>
2022-06-28 21:47:25 -0400

    Create BaseSkeleton that contains styles

--- src/components/shared/elements/Skeletons.tsx
+++ src/components/shared/elements/Skeletons.tsx
@@ -3,11 +3,9 @@ import { rem } from 'polished';
 import { Skeleton } from '@mantine/core';
 import { BackgroundPanel2, BackgroundPanel } from '../../../colors';
 
-export const POAPBadgeSkeleton = (props: React.ComponentProps<typeof Skeleton>) => {
+const BaseSkeleton = (props: React.ComponentProps<typeof Skeleton>) => {
   return (
     <Skeleton
-      height={rem(150)}
-      circle
       sx={(_) => ({
         '&::after': {
           backgroundColor: BackgroundPanel,
@@ -19,41 +17,16 @@ export const POAPBadgeSkeleton = (props: React.ComponentProps<typeof Skeleton>) 
       {...props}
     />
   );
+};
+
+export const POAPBadgeSkeleton = (props: React.ComponentProps<typeof Skeleton>) => {
+  return <BaseSkeleton height={rem(150)} circle {...props} />;
 };
 
 export const ProfileImageSkeleton = (props: React.ComponentProps<typeof Skeleton>) => {
-  return (
-    <Skeleton
-      height={rem(160)}
-      width={rem(160)}
-      circle
-      sx={(_) => ({
-        '&::after': {
-          backgroundColor: BackgroundPanel,
-        },
-        '&::before': {
-          backgroundColor: BackgroundPanel2,
-        },
-      })}
-      {...props}
-    />
-  );
+  return <BaseSkeleton height={rem(160)} width={rem(160)} circle {...props} />;
 };
 
 export const TextSkeleton = (props: React.ComponentProps<typeof Skeleton>) => {
-  return (
-    <Skeleton
-      height={rem(20)}
-      width={rem(200)}
-      sx={(_) => ({
-        '&::after': {
-          backgroundColor: BackgroundPanel,
-        },
-        '&::before': {
-          backgroundColor: BackgroundPanel2,
-        },
-      })}
-      {...props}
-    />
-  );
+  return <BaseSkeleton height={rem(20)} width={rem(200)} {...props} />;
 };
