ff80bdd9ff19e78ee65f8c555ceba8aa94beb3e9
ponyjackal <pony1125@outlook.com>
2022-07-03 20:10:40 -0400

    create RepoHex & RepoHexSkeleton

---
+++ src/components/repos/RepoHex.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import styled from 'styled-components';
+import { rem } from 'polished';
+import { ExtraHover, ExtraPressed, Slate1, TextGray } from '../../colors';
+import { Body, BodyAsAnchor, Hex, InfoHexBase } from '../shared/elements/InfoHexBase';
+import { GitPOAP, Minted, People, Star } from '../shared/elements/icons';
+import { Text } from '../shared/elements/Text';
+import { TitleNoHover } from '../shared/elements/Title';
+import { Repo } from './RepoList';
+import { TextSkeleton } from '../shared/elements';
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: ${rem(0)} ${rem(20)};
+  min-height: ${rem(100)};
+`;
+
+const RepoName = styled(TitleNoHover)`
+  font-size: ${rem(20)};
+  line-height: ${rem(26)};
+`;
+
+const OrgName = styled(TitleNoHover)`
+  font-size: ${rem(14)};
+  line-height: ${rem(26)};
+  color: ${TextGray};
+`;
+
+const Stats = styled.div`
+  align-items: center;
+  display: inline-flex;
+  margin-top: ${rem(10)};
+
+  svg {
+    height: ${rem(16)};
+    fill: ${Slate1};
+    margin-right: ${rem(4)};
+
+    &:not(:first-child) {
+      margin-left: ${rem(10)};
+    }
+  }
+`;
+
+const BODY_HEIGHT = 20;
+
+const InfoHexBaseStyled = styled(InfoHexBase)`
+  ${Body}, ${BodyAsAnchor} {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    margin-top: ${rem(BODY_HEIGHT)};
+    margin-bottom: ${rem(BODY_HEIGHT)};
+    padding: ${rem(15)} 0;
+
+    &:before {
+      height: ${rem(BODY_HEIGHT)};
+      top: ${rem(-BODY_HEIGHT)};
+    }
+    &:after {
+      height: ${rem(BODY_HEIGHT)};
+      bottom: ${rem(-BODY_HEIGHT)};
+    }
+
+    &:hover {
+      ${RepoName} {
+        color: ${ExtraHover};
+      }
+    }
+
+    &:active {
+      ${RepoName} {
+        color: ${ExtraPressed};
+      }
+    }
+  }
+
+  ${Hex} {
+    width: ${rem(280)};
+  }
+`;
+
+export const RepoHexSkeleton = () => {
+  return (
+    <InfoHexBaseStyled>
+      <Content>
+        <RepoName>
+          <TextSkeleton height={rem(26)} style={{ marginBottom: rem(5) }} />
+        </RepoName>
+        <Stats>
+          <TextSkeleton height={rem(18)} width={rem(140)} />
+        </Stats>
+      </Content>
+    </InfoHexBaseStyled>
+  );
+};
+
+type Props = {
+  repo: Repo;
+  starredCount?: number;
+};
+
+export const RepoHex = ({ repo, starredCount }: Props) => {
+  const { id, name, gitPOAPs, claimCount, uniqueContributorCount, organization } = repo;
+
+  return (
+    <InfoHexBaseStyled href={`/rp/${id}`} hoverEffects>
+      <Content>
+        <RepoName>{name}</RepoName>
+        <OrgName>{organization.name}</OrgName>
+        {/* @TODO: Add back once backend changes are ready - Jay (Jul 3 2022) */}
+        {/* <Stats>
+            <People />
+            <Text>{uniqueContributorCount ?? 0}</Text>
+            <GitPOAP />
+            <Text>{gitPOAPs.length}</Text>
+            <Minted />
+            <Text>{claimCount ?? 0}</Text>
+            <Star />
+            <Text>{starredCount ?? 0}</Text>
+          </Stats> */}
+      </Content>
+    </InfoHexBaseStyled>
+  );
+};
