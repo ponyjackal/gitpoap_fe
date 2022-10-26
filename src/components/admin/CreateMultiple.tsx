7da5595e8bbf8564ecf845dea59b5875a029bc7b
ponyjackal <pony1125@outlook.com>
2022-10-26 16:49:22 -0400

    refactor: create CreateMultiple component

---
+++ src/components/admin/CreateMultiple.tsx
@@ -0,0 +1,50 @@
+import React, { useCallback, useState } from 'react';
+import { rem } from 'polished';
+import { HiPlus } from 'react-icons/hi';
+import { Group, Stack } from '@mantine/core';
+import { v4 as uuidv4 } from 'uuid';
+import { Button, Header } from '../shared/elements';
+import { Divider } from '../shared/elements';
+
+import { CreateRow } from './CreateRow';
+
+type Row = { id: string };
+
+export const CreateMultiple = () => {
+  const [rows, setRows] = useState<Row[]>([{ id: uuidv4() }]);
+
+  const deleteRow = useCallback(
+    (id: string) => setRows(rows.filter((row) => row.id !== id)),
+    [rows],
+  );
+
+  return (
+    <Group position="center">
+      <Stack style={{ width: '100%' }}>
+        <Group position="apart" style={{ width: '100%' }}>
+          <Header style={{ alignSelf: 'start' }}>{'Admin - Create New  GitPOAPs'}</Header>
+          <Button
+            leftIcon={<HiPlus size={18} />}
+            onClick={() => setRows([...rows, { id: uuidv4() }])}
+            style={{}}
+          >
+            {`Add Row (${rows.length})`}
+          </Button>
+        </Group>
+        <Divider style={{ width: '100%', marginTop: rem(10), marginBottom: rem(10) }} />
+        {rows.map((row, index) => {
+          return (
+            <CreateRow key={row.id} rowNumber={index + 1} onDelete={deleteRow} rowId={row.id} />
+          );
+        })}
+      </Stack>
+      <Button
+        leftIcon={<HiPlus size={18} />}
+        onClick={() => setRows([...rows, { id: uuidv4() }])}
+        style={{ marginTop: rem(20), marginBottom: rem(35) }}
+      >
+        {`Add Row (${rows.length})`}
+      </Button>
+    </Group>
+  );
+};
--- src/pages/admin/gitpoap/create-multiple.tsx
+++ src/pages/admin/gitpoap/create-multiple.tsx
@@ -1,75 +1,25 @@
-import { useState, useCallback } from 'react';
 import { rem } from 'polished';
-import type { NextPage } from 'next';
 import Head from 'next/head';
-import { HiPlus } from 'react-icons/hi';
-import { Grid, Group, Stack } from '@mantine/core';
-import { v4 as uuidv4 } from 'uuid';
-import { Button, Header } from '../../../components/shared/elements';
-import { Divider } from '../../../components/shared/elements';
-import { CreateRow } from '../../../components/admin/CreateRow';
+import { Grid } from '@mantine/core';
 import { ConnectGitHub } from '../../../components/admin/ConnectGitHub';
+import { CreateMultiple } from '../../../components/admin/CreateMultiple';
 import { useIsAdmin } from '../../../hooks/useIsAdmin';
+import { Page } from '../../_app';
 
-type Row = { id: string };
-
-const CreateMultiple: NextPage = () => {
+const CreateMultipleForm: Page = () => {
   const isAdmin = useIsAdmin();
-  const [rows, setRows] = useState<Row[]>([{ id: uuidv4() }]);
-
-  const deleteRow = useCallback(
-    (id: string) => setRows(rows.filter((row) => row.id !== id)),
-    [rows],
-  );
 
   return (
-    <div>
+    <Grid justify="center" style={{ marginTop: rem(20) }}>
       <Head>
         <title>{'Create Multiple GitPOAPs | GitPOAP'}</title>
         <meta name="description" content="GitPOAP Admin" />
       </Head>
-      <Grid justify="center" style={{ marginTop: rem(20) }}>
-        <Grid.Col xs={10} sm={10} md={10} lg={10} xl={10}>
-          {isAdmin ? (
-            <Group position="center">
-              <Stack style={{ width: '100%' }}>
-                <Group position="apart" style={{ width: '100%' }}>
-                  <Header style={{ alignSelf: 'start' }}>{'Admin - Create New  GitPOAPs'}</Header>
-                  <Button
-                    leftIcon={<HiPlus size={18} />}
-                    onClick={() => setRows([...rows, { id: uuidv4() }])}
-                    style={{}}
-                  >
-                    {`Add Row (${rows.length})`}
-                  </Button>
-                </Group>
-                <Divider style={{ width: '100%', marginTop: rem(10), marginBottom: rem(10) }} />
-                {rows.map((row, index) => {
-                  return (
-                    <CreateRow
-                      key={row.id}
-                      rowNumber={index + 1}
-                      onDelete={deleteRow}
-                      rowId={row.id}
-                    />
-                  );
-                })}
-              </Stack>
-              <Button
-                leftIcon={<HiPlus size={18} />}
-                onClick={() => setRows([...rows, { id: uuidv4() }])}
-                style={{ marginTop: rem(20), marginBottom: rem(35) }}
-              >
-                {`Add Row (${rows.length})`}
-              </Button>
-            </Group>
-          ) : (
-            <ConnectGitHub />
-          )}
-        </Grid.Col>
-      </Grid>
-    </div>
+      <Grid.Col xs={10} sm={10} md={10} lg={10} xl={10}>
+        {isAdmin ? <CreateMultiple /> : <ConnectGitHub />}
+      </Grid.Col>
+    </Grid>
   );
 };
 
-export default CreateMultiple;
+export default CreateMultipleForm;
